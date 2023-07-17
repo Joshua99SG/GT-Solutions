afb884aaecf1384527b568141405bb494ba66f85
root <joshuasg99@gmail.com>
2023-07-17 15:58:51 +0000

    develop #commit changes for production URL

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 // Configuración de la base de datos
 define('DB_HOST', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '180899sgSG@');
 define('DB_NAME', 'gt_solutions');
 ?>
